35daeebe63a03134013cf460f6a3fed2111dfff9
tsubasaogawa <hurikake@gmail.com>
2023-06-30 23:38:44 +0900

    make it possible to handle dynamo

--- src/models/item/main.go
+++ src/models/item/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
 )
 
-type widget struct {
-	Id        string `dynamo:"id"`
-	Timestamp int    `dynamo:"timestamp"`
-	Title     string `dynamo:"title"`
-	Camera    string `dynamo:"camera"`
-	Lens      string `dynamo:"lens"`
-	Exposure  string `dynamo:"exposure"`
-	F         int    `dynamo:"f"`
+type Table struct {
+	dynamo.Table
+}
+
+type Item struct {
+	Id        string
+	Timestamp int
+	Title     string
+	Camera    string
+	Lens      string
+	Exposure  string
+	F         float64
+}
+
+const (
+	REGION = "ap-northeast-1"
+	TABLE  = "photo.ogatube.com-item"
+)
+
+func New() *Table {
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &aws.Config{
+		Region: aws.String(REGION),
+	})
+
+	return &Table{db.Table(TABLE)}
+}
+
+func (t *Table) _Get(id string) *Item {
+	item := Item{}
+
+	if err := t.Get("Id", id).One(&item); err != nil {
+		fmt.Errorf("foo")
+	}
+
+	return &item
 }
