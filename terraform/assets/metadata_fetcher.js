6d13914f5a22f01197caef013baae566e2bb95ff
tsubasaogawa <hurikake@gmail.com>
2024-05-31 23:26:34 +0900

    feat: add metadata to image footer

--- terraform/assets/metadata_fetcher.js
+++ terraform/assets/metadata_fetcher.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    try {
-        // <img> タグを取得
-        const img = document.getElementById("targetImage");
-        if (img) {
-            // 画像ファイル名を取得
-            const src = img.getAttribute("src");
-            const fileName = "blog/IMG_20240508_130440.jpg"; // src.split('/').pop();
+    const apiEndpoint = "https://photoview.ogatube.com/metadata";
+    const imgs = document.getElementsByClassName("load-exif");
+    const getPathFromUrl = function (url) {
+        return url.split("/").slice(3).join("/");
+    };
+    const formatMetadata = function (data) {
+        return `${data["camera"]} (${data["lens"]}), ${data["focal_length"]}mm, f/${data["f"]}, ISO ${data["iso"]}, ${data["shutter_speed"]} sec, ${data["exposure"]} EV`;
+    };
 
-            // API エンドポイント
-            const apiEndpoint = "https://photoview.ogatube.com/metadata";
+    for (let img of imgs) {
+        const src = img.getAttribute("src");
 
-            // API へのリクエスト
-            const response = await fetch(`${apiEndpoint}/${fileName}`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            const data = await response.json();
-
-            // レスポンスデータを表示する要素を作成
-            // const metadataDiv = document.createElement("div");
-            // metadataDiv.innerText = `Metadata: ${JSON.stringify(data)}`;
-
-            // <img> タグのすぐ下に挿入
-            // img.parentNode.insertBefore(metadataDiv, img.nextSibling);
-            console.log(JSON.stringify(data));
+        const response = await fetch(`${apiEndpoint}/${getPathFromUrl(src)}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
         }
-    } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+        const metadata = await response.json();
+
+        const metadataDiv = document.createElement("div");
+        metadataDiv.innerHTML = `<i>${formatMetadata(metadata)}</i>`;
+
+        img.parentNode.insertBefore(metadataDiv, img.nextSibling);
     }
 });
