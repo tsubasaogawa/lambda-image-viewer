85f3b1b62330cd4958816f45d30f7191790992e9
tsubasaogawa <hurikake@gmail.com>
2024-06-06 22:23:57 +0900

    feat: improve processing speed

--- terraform/assets/metadata_fetcher.js
+++ terraform/assets/metadata_fetcher.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    const apiEndpoint = "https://photoview.ogatube.com/metadata";
+    if (typeof mfApiEndpoint === 'undefined') {
+        throw new Error('mfApiEndpoint is required');
+    }
+
     const imgs = document.getElementsByClassName("load-exif");
     const getPathFromUrl = function (url) {
         return url.split("/").slice(3).join("/");
@@ -8,18 +11,21 @@ document.addEventListener("DOMContentLoaded", async function () {
         return `${data["camera"]} (${data["lens"]}), ${data["focal_length"]}mm, f/${data["f"]}, ISO ${data["iso"]}, ${data["shutter_speed"]} sec, ${data["exposure"]} EV`;
     };
 
-    for (let img of imgs) {
+    const requests = Array.from(imgs).map(async (img) => {
         const src = img.getAttribute("src");
 
-        const response = await fetch(`${apiEndpoint}/${getPathFromUrl(src)}`);
+        const response = await fetch(`${mfApiEndpoint}/${getPathFromUrl(src)}`);
         if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
+            console.error('Network response was not ok: ' + response.statusText);
         }
         const metadata = await response.json();
 
         const metadataDiv = document.createElement("div");
+        metadataDiv.className = 'exif';
         metadataDiv.innerHTML = `<i>${formatMetadata(metadata)}</i>`;
 
         img.parentNode.insertBefore(metadataDiv, img.nextSibling);
-    }
+    });
+
+    await Promise.all(requests);
 });
