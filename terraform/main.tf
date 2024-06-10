4ea122f25883e8b92abadebb49bd8ada983e259f
tsubasaogawa <hurikake@gmail.com>
2024-06-10 13:31:53 +0900

    fix: enable cache

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,7 +84,7 @@ resource "aws_cloudfront_distribution" "viewer" {
       "GET",
       "HEAD",
     ]
-    cache_policy_id = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # CachingOptimized
     cached_methods = [
       "GET",
       "HEAD",
